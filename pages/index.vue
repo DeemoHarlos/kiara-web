005a2a33214d5944996f6f6f16a29b9c251b493a
DeemoHarlos <cces4415@gmail.com>
2021-08-19 19:14:07 +0800

    Guess what? We are going with Nuxt! :D

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  Tutorial
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
---
+++ pages/kiara.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  div
+    NuxtLink(to="/kiara/temp") temp
+    NuxtChild
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
