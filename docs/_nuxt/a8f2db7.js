b57daceccc83a2778f84da96f853cab8f48896d3
DeemoHarlos <cces4415@gmail.com>
2021-09-23 17:20:53 +0800

    build 2021 Sep 23

---
+++ docs/_nuxt/a8f2db7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{345:function(t,e,n){"use strict";n.r(e);var r=n(21).default.extend({mounted:function(){this.$router.push("/kiara/")}}),l=n(78),component=Object(l.a)(r,(function(){var t=this.$createElement;return(this._self._c||t)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
