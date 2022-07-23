93f3e8a12c0715c56cd24c3e807dda8fdedec0fb
DeemoHarlos <cces4415@gmail.com>
2022-07-23 18:08:59 +0800

    rwd

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -2,18 +2,31 @@
   #navbar.flex-row.middle-center.w-100.px-3.px-lg-5.py-2
     span#navbar-name.mr-3.mr-lg-5
       NuxtLink(to="/") Takanashi Kiara
-    template(v-for="(page, index) in pages")
-      .navline-line(v-if="index")
-      NuxtLink.navlink.flex-fill.mx-3.text-center.text-white(:to="page.route")
-        div.flex-row-center.position-relative.h-100.py-2(:class="{ current: $route.path.startsWith(page.route) }")
-          span.h6.w-100.m-0.pb-1 {{ page.text }}
+    .d-none.d-md-flex.flex-row.middle-center.flex-fill
+      template(v-for="(page, index) in pages")
+        .navline-line(v-if="index")
+        NuxtLink.navlink.flex-fill.mx-3.text-center.text-white(:to="page.route")
+          div.flex-row-center.position-relative.h-100.py-2(:class="{ current: $route.path.startsWith(page.route) }")
+            span.h6.w-100.m-0.pb-1 {{ page.text }}
+    .d-flex.d-md-none.flex-fill
+    .d-flex.d-md-none.mr-5.flex-row
+      .navlink.mx-3.text-center.text-white(v-if="thisPage" @click="triggerNavbar")
+        div.flex-row-center.position-relative.h-100.py-2(class="current")
+          span.h6.w-100.m-0.pb-1 {{ thisPage.text }}
+      #sidebar.position-fixed(:class="{ open: navbarOpen }" @click="triggerNavbar")
+        template(v-for="(page, index) in pages")
+          .navline-h(v-if="index")
+          NuxtLink.navlink.flex-fill.mx-5.text-center.text-white(:to="page.route")
+            div.flex-row-center.position-relative.h-100.py-2(:class="{ current: $route.path.startsWith(page.route) }")
+              span.h6.w-100.m-0.pb-1 {{ page.text }}
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { computed, defineComponent, ref, useRoute } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   setup() {
+    const route = useRoute()
     const pages = [
       { route: '/intro', text: 'Intro' },
       { route: '/lore', text: 'Lore' },
@@ -23,7 +36,10 @@ export default defineComponent({
       { route: '/music', text: 'Music' },
       { route: '/kfp', text: 'KFP' },
     ]
-    return { pages }
+    const thisPage = computed(() => pages.find(p => route.value.path.startsWith(p.route)))
+    const navbarOpen = ref(false)
+    const triggerNavbar = () => { navbarOpen.value = !navbarOpen.value }
+    return { pages, thisPage, navbarOpen, triggerNavbar }
   },
 })
 </script>
@@ -43,8 +59,14 @@ $transition-time: 300ms
 
   #navbar-name
     font-size: 2rem
-    @media screen and (max-width: 992px)
-      font-size: 1.5rem
+
+  #sidebar
+    background-color: $kiara-green
+    top: 65px
+    left: 100%
+    width: 300px
+    padding: 1rem 5rem
+    z-index: 300
 
   .navline-line
     width: 1px
@@ -53,7 +75,15 @@ $transition-time: 300ms
     background-color: $line-color
     transform: rotate(33deg)
 
+  .navline-h
+    height: 1px
+    background-color: $line-color
+
   .navlink
+    & + #sidebar.open
+      left: unset
+      right: 0
+
     div
       transition: background-color $transition-time
       background-color: transparent
